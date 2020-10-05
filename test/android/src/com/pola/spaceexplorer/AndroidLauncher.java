35c122122fcdd74fa77bf7c125b4cb6a0773ae3c
Gabriel Henrique Silva <ghsilha@gmail.com>
2020-10-05 14:34:07 -0300

    added libgdx

---
+++ test/android/src/com/pola/spaceexplorer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pola.spaceexplorer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pola.spaceexplorer.SpaceExplorer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceExplorer(), config);
+	}
+}
---
+++ test/desktop/src/com/pola/spaceexplorer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pola.spaceexplorer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pola.spaceexplorer.SpaceExplorer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceExplorer(), config);
+	}
+}
